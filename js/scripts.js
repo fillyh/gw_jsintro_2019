c236cb8eb273e75fe4a1145f97e48baa1a69015d
felipe hernandez <felipehernandez@felipes-MacBook-Pro.local>
2019-08-12 20:58:14 -0700

    day 1  exercise

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+
+// function myFunk(){
+//     let question = prompt('Tell me your name?');
+//     console.log('Your name is ' + question);
+// }
+
+function myFunk(){
+    let question = prompt('Enter a hobby...');
+    console.log('You like ' + question);
+    
+    alert('OMG I like ' + question + ' too! ')
+    console.log(alert);
+}
+
+
+
+// function myFunk(){
+//     let question = alert('Tell me your name?');
+// }
+
+console.log('My Hobbies');
+console.log('PS4 Gaming');
+console.log( 'Hiking');
+console.log( 'Coding');
