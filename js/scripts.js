b48460b1cc585dcdf3942cdb7767eb20299f2d1c
felipe hernandez <felipehernandez@felipes-MacBook-Pro.local>
2019-08-14 19:53:08 -0700

    day 2 in class exercise

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+// var firstName = 'Jacque';
+// var age = 26;
+
+//long way
+// console.log('My name is ' + firstName + ' and my age is ' + age);
+
+//shorter way
+// console.log(`My name is ${firstName} and my age is ${age}`);
+
+// alert(`My name is ${firstName} and my age is ${age}`);
+
+// let hello = new String('Hello Friends');
+// alert (hello);
+
+// let todayDate = new Date();
+// alert(todayDate);
+
+
+// let hello = 'Hello There';
+// alert(hello);
+// console.log(typeof (hello));
+// console.log(hello.length);
+
+
+// const topThreeMovies = ['Blade', 'Avengers', 'Alita'];
+// console.log(`my three top movies are: ${topThreeMovies}`);
+// console.log(`my main movie is: ${topThreeMovies[1]}`);
+// console.log(`this is a type of: ${typeof(topThreeMovies)}`);
+
+// ------- In Class Assignment ---------
+
+// Asks for the user's first name
+// Asks for the user's last name
+
+// Logs the user's first name to the console
+
+// Alerts the user's last name
+
+// *Asks for the user's birthday, confirms whether the birthday recieved is correct, and alerts the resulting birthday
